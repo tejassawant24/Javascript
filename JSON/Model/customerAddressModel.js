ab8df9e9b686d6aa7bef6b3c6fb47c55401d3028
Tejas Sawant <tejassawant@Tejass-MacBook-Air.local>
2019-08-06 00:16:57 +0530

    Serialization and Deserialization of JSON

---
+++ JSON/Model/customerAddressModel.js
@@ -0,0 +1,10 @@
+function customerAddressModel(){
+    
+    this.flatNo=undefined;
+    this.buildingName=undefined;
+    this.area=undefined;
+    this.landmark=undefined;
+    this.cityName=undefined;
+    this.state=undefined;
+    this.pinCode=undefined;
+}
---
+++ JSON/Model/customerModel.js
@@ -0,0 +1,14 @@
+
+function customerModel(){
+    this.firstName=undefined;
+    this.lastName=undefined;
+    this.age=undefined;
+    this.dataOfBirth=undefined;
+
+    //One to One relationship
+    this.customerCommunication=undefined;
+    this.customerAddress=undefined;
+
+
+    this.customerCommunicationList=undefined;
+}
